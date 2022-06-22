e4c833f3c46fe306f452beaccb8f0a24813b407f
mendoza2929 <reuelmendoza29@gmail.com>
2022-06-22 13:05:01 +0800

    done home page but not picture yet

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './home.css'
 import {BsArrowRightSquare} from 'react-icons/bs'
+import Title from './Title'
 
 
 
@@ -11,16 +12,16 @@ const Hero = () => {
     <section className="hero">
       <div className="container">
         <div className="row">
-          <h3>Welcome to dotDesign</h3>
-          <h1>Best UI/UX Design Services</h1>
-          <p>Do you want to work on your product with a team that follows a well-defined design process, meets deadlines, and delivers flawless results? Use Intellectsoft's services for user interface and user experience. Our design team is a small design studio within a large software company that can help you create an engaging product quickly and easily.</p>
+        <Title subtitle={'Welcome to dotDesign'} title={'Best UI/UX design'}/>
+          <p>Do you want to work on your product with a team that follows a well-defined design process, meets deadlines, and delivers flawless results?. Our design team is a small design studio within a large software company that can help you create an engaging product quickly and easily.</p>
         <div className="button">
           <button >
-            GET STARTED NOW <BsArrowRightSquare className='icon-btn'/>
+            GET STARTED NOW <i><BsArrowRightSquare/></i>
           </button>
           <button className="primary-btn">
-          View Projects<BsArrowRightSquare className='icon-btn'/>
+          View Projects <i><BsArrowRightSquare/></i>
           </button>
+     
         </div>
         </div>
       </div>
